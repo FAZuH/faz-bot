080bd311f23fc6e2bb6bc8bf4098f22c01eec37c
FAZuH <fazuhhh@gmail.com>
2024-08-20 14:50:35 +0700

    test(fazutil.db): add DiscordGuildRepository and DiscordUserRepository tests

---
+++ tests/fazutil/db/fazbot/test_discord_guild_repository.py
@@ -0,0 +1,26 @@
+from typing import override
+
+from fazutil.db.fazbot.repository import DiscordGuildRepository
+
+from ._common_fazbot_repository_test import CommonFazbotRepositoryTest
+
+
+class TestDiscordGuildRepository(
+    CommonFazbotRepositoryTest.Test[DiscordGuildRepository]
+):
+
+    @override
+    def _get_mock_data(self):
+        model = self.repo.model
+        mock_data1 = model(guild_id=1, guild_name="a")
+        mock_data2 = mock_data1.clone()
+        mock_data3 = mock_data1.clone()
+        mock_data3.guild_id = 2
+        mock_data4 = mock_data1.clone()
+        mock_data4.guild_name = "b"
+        return (mock_data1, mock_data2, mock_data3, mock_data4, "guild_name")
+
+    @property
+    @override
+    def repo(self):
+        return self.database.discord_guild_repository
---
+++ tests/fazutil/db/fazbot/test_discord_user_repository.py
@@ -0,0 +1,24 @@
+from typing import override
+
+from fazutil.db.fazbot.repository import DiscordUserRepository
+
+from ._common_fazbot_repository_test import CommonFazbotRepositoryTest
+
+
+class TestDiscordUserRepository(CommonFazbotRepositoryTest.Test[DiscordUserRepository]):
+
+    @override
+    def _get_mock_data(self):
+        model = self.repo.model
+        mock_data1 = model(user_id=1, username="a")
+        mock_data2 = mock_data1.clone()
+        mock_data3 = mock_data1.clone()
+        mock_data3.user_id = 2
+        mock_data4 = mock_data1.clone()
+        mock_data4.username = "b"
+        return (mock_data1, mock_data2, mock_data3, mock_data4, "username")
+
+    @property
+    @override
+    def repo(self):
+        return self.database.discord_user_repository
