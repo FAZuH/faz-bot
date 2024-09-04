b3b4d7acc0f021a6f0e619d5ab46cfa842a1c933
FAZuH <fazuhhh@proton.me>
2024-09-04 13:46:17 +0700

    refactor(test): refactor TaskDbInsert test
    - Move ResponseHandler into its own file as TaskDbInsert has too many
    responsibilities.
    
    - Split test_run into test on empty response list, and test on non-empty
    response list
    
    - Add test_run_insert_fazdb_uptime

--- tests/fazdb/heartbeat/task/test_request_queue.py
+++ tests/fazdb/heartbeat/task/test_request_queue.py
@@ -1,134 +1,138 @@
-import unittest
+from collections.abc import Coroutine
 from datetime import datetime as dt
+from unittest import TestCase
+from unittest.mock import MagicMock
 
 from fazdb.heartbeat.task.request_queue import RequestQueue
 
 
-class TestRequestList(unittest.TestCase):
+class TestRequestList(TestCase):
 
     def setUp(self) -> None:
         self.request_list = RequestQueue()
 
-    async def mock_coro(self, arg: str) -> str:
-        return arg
-
     def test_enqueue_and_dequeue(self) -> None:
-        # sourcery skip: class-extract-method
-        # PREPARE
-        test_coro1 = self.mock_coro("foo")
+        # Prepare
+        mock_coro1 = self.__create_mock_coro("foo")
         test_req_ts1 = dt.now().timestamp() - 100
 
-        # ACT
-        self.request_list.enqueue(test_req_ts1, test_coro1)
+        # Act
+        self.request_list.enqueue(test_req_ts1, mock_coro1)
 
-        # ASSERT
+        # Assert
         # NOTE: Assert that the request is enqueued properly
         self.assertEqual(len(self.request_list._list), 1)
-        self.assertEqual(self.request_list._list[0].coro, test_coro1)
+        self.assertEqual(self.request_list._list[0].coro, mock_coro1)
         self.assertEqual(self.request_list._list[0]._req_ts, test_req_ts1)
 
-        # ACT
+        # Act
         result = self.request_list.dequeue(1)
 
-        # ASSERT
+        # Assert
         # NOTE: Assert that the correct request is dequeued
         self.assertEqual(len(result), 1)
-        self.assertEqual(result[0], test_coro1)
+        self.assertEqual(result[0], mock_coro1)
 
     def test_dequeue_with_request_ts(self) -> None:
-        # PREPARE
-        test_coro1 = self.mock_coro("foo")
-        test_coro2 = self.mock_coro("bar")
+        # Prepare
+        mock_coro1 = self.__create_mock_coro("foo")
+        mock_coro2 = self.__create_mock_coro("bar")
         test_req_ts1 = dt.now().timestamp() - 100
         test_req_ts2 = dt.now().timestamp() - 200  # earlier timestamp
 
-        self.request_list.enqueue(test_req_ts1, test_coro1)
-        self.request_list.enqueue(test_req_ts2, test_coro2)
+        self.request_list.enqueue(test_req_ts1, mock_coro1)
+        self.request_list.enqueue(test_req_ts2, mock_coro2)
 
-        # ACT
+        # Act
         result = self.request_list.dequeue(1)
 
-        # ASSERT
+        # Assert
         # NOTE: Assert that the request with the earliest timestamp is dequeued first
         self.assertEqual(len(result), 1)
-        self.assertEqual(result[0], test_coro2)
+        self.assertEqual(result[0], mock_coro2)
         remaining_item = self.request_list._list.pop()
         # NOTE: Assert that the remaining item is the one with the later timestamp
-        self.assertEqual(remaining_item.coro, test_coro1)
+        self.assertEqual(remaining_item.coro, mock_coro1)
         self.assertEqual(remaining_item._req_ts, test_req_ts1)
 
     def test_dequeue_with_priority(self) -> None:
-        # PREPARE
-        test_coro1 = self.mock_coro("foo")
-        test_coro2 = self.mock_coro("bar")
+        # Prepare
+        mock_coro1 = self.__create_mock_coro("foo")
+        mock_coro2 = self.__create_mock_coro("bar")
         test_req_ts1 = dt.now().timestamp() - 100
         test_req_ts2 = dt.now().timestamp() - 50  # should still return higher priority
-        self.request_list.enqueue(test_req_ts1, test_coro1, priority=100)
+        self.request_list.enqueue(test_req_ts1, mock_coro1, priority=100)
         self.request_list.enqueue(
-            test_req_ts2, test_coro2, priority=200
+            test_req_ts2, mock_coro2, priority=200
         )  # higher priority
 
-        # ACT
+        # Act
         result = self.request_list.dequeue(1)
 
-        # ASSERT
+        # Assert
         self.assertEqual(len(result), 1)
-        self.assertEqual(result.pop(), test_coro2)
+        self.assertEqual(result.pop(), mock_coro2)
 
     def test_dequeue_equal_all(self) -> None:
-        # PREPARE
-        test_coros = [self.mock_coro(str(i)) for i in range(100)]
+        # Prepare
+        test_coros = [self.__create_mock_coro(str(i)) for i in range(100)]
         test_req_ts = 0
         for coro in test_coros:
             self.request_list.enqueue(test_req_ts, coro)
 
-        # ACT
+        # Act
         result = self.request_list.dequeue(15)
 
-        # ASSERT
+        # Assert
         self.assertEqual(len(result), 15)
         for coro in result:
             self.assertTrue(coro in test_coros)
 
     def test_request_item_is_eligible(self) -> None:
-        # PREPARE
-        test_coro1 = self.mock_coro("foo")
+        # Prepare
+        mock_coro1 = self.__create_mock_coro("foo")
         test_req_ts1 = dt.now().timestamp() - 1  # Past timestamp
-        request_item = RequestQueue.RequestItem(test_coro1, 100, test_req_ts1)
+        request_item = RequestQueue.RequestItem(mock_coro1, 100, test_req_ts1)
 
-        # ASSERT
+        # Assert
         self.assertTrue(request_item.is_eligible())
 
     def test_request_item_is_not_eligible(self) -> None:
-        # PREPARE
-        test_coro1 = self.mock_coro("foo")
+        # Prepare
+        mock_coro1 = self.__create_mock_coro("foo")
         test_req_ts1 = dt.now().timestamp() + 1000  # Future timestamp
-        request_item = RequestQueue.RequestItem(test_coro1, 100, test_req_ts1)
+        request_item = RequestQueue.RequestItem(mock_coro1, 100, test_req_ts1)
 
-        # ASSERT
-        # NOTE: Assert that request item is not eligible
+        # Assert
         self.assertFalse(request_item.is_eligible())
 
     def test_request_item_comparison(self) -> None:
-        # PREPARE
-        test_coro1 = self.mock_coro("foo")
-        test_coro2 = self.mock_coro("bar")
-        request_item1 = RequestQueue.RequestItem(test_coro1, 100, 0)
-        request_item2 = RequestQueue.RequestItem(test_coro2, 200, 0)
+        # Prepare
+        mock_coro1 = self.__create_mock_coro("foo")
+        mock_coro2 = self.__create_mock_coro("bar")
+        item1 = RequestQueue.RequestItem(mock_coro1, 100, 0)
+        item2 = RequestQueue.RequestItem(mock_coro2, 200, 0)
 
-        # ASSERT
-        self.assertLess(request_item2, request_item1)
+        # Assert
+        self.assertLess(item2, item1)
 
     def test_request_item_equality(self) -> None:
-        # PREPARE
-        test_coro1 = self.mock_coro("foo")
-        test_coro2 = self.mock_coro("foo")
-        request_ts = dt.now().timestamp()
-        request_item1 = RequestQueue.RequestItem(test_coro1, 100, request_ts)
-        request_item2 = RequestQueue.RequestItem(test_coro2, 100, request_ts)
+        # Prepare
+        mock_coro1 = self.__create_mock_coro("foo")
+        mock_coro2 = self.__create_mock_coro("foo")
+        item1 = RequestQueue.RequestItem(mock_coro1, 100, 100)
+        item2 = RequestQueue.RequestItem(mock_coro2, 100, 100)
 
-        # ASSERT
-        self.assertEqual(request_item1, request_item2)
+        # Assert
+        self.assertEqual(item1, item2)
+
+    @staticmethod
+    def __create_mock_coro(ret: str) -> MagicMock:
+        mock = MagicMock()
+        mock.return_value = ret
+        mock.cr_frame.f_locals = ret
+        mock.__class__ = MagicMock
+        return mock
 
     def tearDown(self) -> None:
         pass
---
+++ tests/fazdb/heartbeat/task/test_response_handler.py
@@ -0,0 +1,186 @@
+from datetime import datetime
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from fazdb.heartbeat.task._response_handler import ResponseHandler
+from fazdb.heartbeat.task.request_queue import RequestQueue
+from fazutil.api.wynn.wynn_api import WynnApi
+
+
+class TestResponseHandler(TestCase):
+
+    def setUp(self) -> None:
+        self._api = MagicMock(spec_set=WynnApi)
+        self.__request_list = MagicMock(spec_set=RequestQueue)
+        self._manager = ResponseHandler(self._api, self.__request_list)
+
+    # OnlinePlayerResponse
+    def test_process_new_response(self) -> None:
+        # Prepare
+        uuid0 = "0"
+        uuid1 = "1"
+        uuid2 = "2"
+        datetime0 = MagicMock(spec_set=datetime)
+        datetime1 = MagicMock(spec_set=datetime)
+        resp0 = MagicMock()
+        resp1 = MagicMock()
+        resp0.body.players = {uuid0, uuid1}
+        resp0.headers.to_datetime.return_value = datetime0
+        resp1.body.players = {uuid1, uuid2}
+        resp1.headers.to_datetime.return_value = datetime1
+
+        # Act
+        self._manager._process_onlineplayers_response(resp0)
+
+        # Assert
+        # NOTE: Assert that player 1 and 2 has logged on.
+        self.assertSetEqual(self._manager.logged_on_players, {uuid0, uuid1})
+        # NOTE: Assert that player 1 and 2 is online, with the correct logged on datetime.
+        self.assertDictEqual(
+            self._manager.online_players, {uuid0: datetime0, uuid1: datetime0}
+        )
+
+        # Act
+        self._manager._process_onlineplayers_response(resp1)
+
+        # Assert
+        # NOTE: Assert that only player 2 has logged on because player 1 is already logged on.
+        self.assertSetEqual(self._manager._logged_on_players, {uuid2})
+        # NOTE: Assert that player 0 is no longer online, while player 1 and player 2 has the correct logged on datetime.
+        self.assertDictEqual(
+            self._manager.online_players, {uuid1: datetime0, uuid2: datetime1}
+        )
+
+    def test_enqueue_player_stats(self) -> None:
+        # Prepare
+        uuid0 = "player0"
+        self._manager._logged_on_players = {uuid0}
+
+        # Act
+        self._manager._enqueue_player()
+
+        # Assert
+        # NOTE: Assert that the correct player is being queued.
+        self._api.player.get_full_stats.assert_called_once_with(uuid0)
+        # NOTE: Assert that enqueue is called with the correct arguments.
+        self.__request_list.enqueue.assert_called_once_with(
+            0, self._api.player.get_full_stats()
+        )
+
+    def test_requeueonline_players(self) -> None:
+        # Prepare
+        resp = MagicMock()
+        resp.headers.expires.to_datetime().timestamp.return_value = 69
+
+        # Act
+        self._manager._requeue_onlineplayers(resp)
+
+        # Assert
+        # NOTE: Assert that enqueue is called with the correct arguments.
+        self.__request_list.enqueue.assert_called_once_with(
+            69, self._api.player.get_online_uuids(), priority=999
+        )
+
+    # PlayerResponse
+    def test_process_player_response(self) -> None:
+        # Prepare
+        guild0 = "guild0"
+        guild1 = "guild1"
+        uuid0 = "0"
+        uuid1 = "1"
+        uuid2 = "2"
+        mock1 = MagicMock()
+        mock2 = MagicMock()
+        mock3 = MagicMock()
+        mock1.body.guild.name = guild0
+        mock1.body.uuid.uuid = uuid0
+        mock1.body.online = True
+        mock2.body.guild.name = guild0
+        mock2.body.uuid.uuid = uuid1
+        mock2.body.online = True
+        mock3.body.guild.name = guild1
+        mock3.body.uuid.uuid = uuid2
+        mock3.body.online = True
+
+        # Act
+        self._manager._process_player_response([mock1])
+
+        # Assert
+        # NOTE: Assert that guild "test0" is logged on.
+        self.assertSetEqual(self._manager._logged_on_guilds, {guild0})
+        # NOTE: Assert that guild "test0" is online with correct players.
+        self.assertDictEqual(self._manager.online_guilds, {guild0: {uuid0}})
+
+        # Act
+        self._manager._process_player_response([mock2, mock3])
+
+        # Assert
+        # NOTE: Assert that only guild test1 is logged on, because test0 is already logged on.
+        self.assertSetEqual(self._manager._logged_on_guilds, {guild1})
+        # NOTE: Assert that both guilds are online with correct players.
+        self.assertDictEqual(
+            self._manager.online_guilds, {guild0: {uuid0, uuid1}, guild1: {uuid2}}
+        )
+
+        # Prepare
+        mock3.body.online = False
+
+        # Act
+        self._manager._process_player_response([mock3])
+
+        # Assert
+        # NOTE: Assert that guild test1 is no longer online.
+        self.assertDictEqual(self._manager.online_guilds, {guild0: {uuid0, uuid1}})
+
+    def test_enqueue_guild(self) -> None:
+        # Prepare
+        guild0 = "guild0"
+        self._manager._logged_on_guilds = {guild0}
+
+        # Act
+        self._manager._enqueue_guild()
+
+        # Assert
+        # NOTE: Assert that the correct guild is being queued.
+        self._api.guild.get.assert_called_once_with(guild0)
+        # NOTE: Assert that enqueue is called with the correct arguments.
+        self.__request_list.enqueue.assert_called_once_with(0, self._api.guild.get())
+
+    def test_requeue_player(self) -> None:
+        # Prepare
+        resp1 = MagicMock()
+        resp2 = MagicMock()  # continued
+        resp1.body.online = True
+        resp1.headers.expires.to_datetime().timestamp.return_value = 69
+        resp2.body.online = False
+
+        # Act
+        self._manager._requeue_player([resp2, resp1])
+
+        # Assert
+        # NOTE: Assert that self._api.player.get_online_uuids() is called with the correct arguments
+        self._api.player.get_full_stats.assert_called_once()
+        # NOTE: Assert that enqueue is called with the correct arguments.
+        self.__request_list.enqueue.assert_called_once_with(
+            69, self._api.player.get_full_stats()
+        )
+
+    # GuildResponse
+    def test_requeue_guild(self) -> None:
+        # Prepare
+        test_name1 = "guild0"
+        test_resp1 = MagicMock()
+        test_resp2 = MagicMock()  # continued
+        test_resp1.body.name = test_name1
+        test_resp1.body.members.get_online_members.return_value = 1
+        test_resp1.headers.expires.to_datetime().timestamp.return_value = 69
+        test_resp2.body.members.get_online_members.return_value = 0
+
+        # Act
+        self._manager._requeue_guild([test_resp1, test_resp2])
+
+        # Assert
+        # NOTE: Assert that self._api.guild.get() is called with the correct arguments
+        self._api.guild.get.assert_called_once_with(test_name1)
+        # NOTE: Assert that enqueue is called with the correct arguments.
+        self.__request_list.enqueue.assert_called_once_with(69, self._api.guild.get())
