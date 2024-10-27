67bad15c31c23afac922b5843877982ed916a890
FAZuH <fazuhhh@proton.me>
2024-10-27 11:26:49 +0700

    chore: misc changes

---
+++ .archive/18ebc29227ef_1_1_2_test.py
@@ -0,0 +1,24 @@
+"""1.1.2 test
+
+Revision ID: 18ebc29227ef
+Revises: 59449aeb3da3
+Create Date: 2024-10-27 01:02:24.495983
+
+"""
+
+from typing import Sequence, Union
+
+
+# revision identifiers, used by Alembic.
+revision: str = "18ebc29227ef"
+down_revision: Union[str, None] = "59449aeb3da3"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ .archive/d7ae7537090b_1_1_3_test.py
@@ -0,0 +1,24 @@
+"""1.1.3 test
+
+Revision ID: d7ae7537090b
+Revises: 18ebc29227ef
+Create Date: 2024-10-27 01:02:27.484118
+
+"""
+
+from typing import Sequence, Union
+
+
+# revision identifiers, used by Alembic.
+revision: str = "d7ae7537090b"
+down_revision: Union[str, None] = "18ebc29227ef"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
