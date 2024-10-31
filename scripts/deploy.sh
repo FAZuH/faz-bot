28d03d995ea4c69c31ea886026dfa1bda27e6589
FAZuH <108622606+FAZuH@users.noreply.github.com>
2024-10-31 16:06:05 +0700

    fix: Fix deploy workflow (#58)
    * fix(ci): fix deploy workflow
    
    * fix(ci): wait for mysql to finish initializing on deploy.sh
    
    * chore(script): minor script changes
    
    On update.sh, use git pull instead of git pull origin main to pull from
    remote tracking branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,20 +14,27 @@ set -e
 
 
 main() {
+    # 1. Make sure database is running before updating it. Needed for step 2
     $COMPOSE up mysql -d
+    "docker/wait-for-it.sh" -t 5 mysql:3306 -- echo "MySQL is ready!"
 
+    # 2. Pull git changes and update database.
+    # This is to ensure scripts and docker configs are 
+    # in the latest version before running docker-compose pull
     $UPDATE_SCRIPT
 
     echo "Pulling latest images..."
+    # 3. Pull latest docker images
     $COMPOSE pull 
 
-    echo "Stopping and removing existing containers..."
-    $COMPOSE down
-
     echo "Starting new containers..."
-    $COMPOSE up -d
+    # 4. Start services
+    # Note that docker will only recreate containers 
+    # if the image (or docker-compose config) has changed
+    $COMPOSE up mysql fazcollect fazcord -d
 
     echo "Removing old images..."
+    # 5. Remove old images (if any)
     docker image prune -f
 
     echo "Update completed successfully."
