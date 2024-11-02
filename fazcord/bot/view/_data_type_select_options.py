a232063fe1412a39ba421619b6e5ad37fee4965d
FAZuH <fazuhhh@proton.me>
2024-11-02 13:48:59 +0700

    chore(bot): remove CharacterSelect and DataTypeSelect

--- fazcord/bot/view/_data_type_select_options.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class DataTypeSelectOptions(Enum):
-    """Mapping of enum member to display value for DataTypeSelect."""
-
-    CATEGORICAL = "Categorical"
-    NUMERICAL = "Numerical"
